821a39f01548c22a781843a44a9441d519b6d70d
Kaan Koyuncu <koyuncukaan@gmail.com>
2023-12-03 23:08:12 +0300

    add printWorkDir func

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func printWorkDir() {
+	var workDir, currDir string
+	projectPrefix := "fe_web_"
+	fmt.Println("Enter work directory: ")
+	fmt.Scanln(&workDir)
+	err := os.Chdir(workDir)
+	if err != nil {
+		fmt.Println(err)
+		printWorkDir()
+	}
+	currDir, _ = os.Getwd()
+	fmt.Println("Current directory: ", currDir)
+
+	entries, err := os.ReadDir(currDir)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, entry := range entries {
+		fmt.Println(entry.Name()[:4])
+		if entry.IsDir() && entry.Name()[:7] == projectPrefix {
+			fmt.Println(entry)
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	asciiArt := `
+  _____           .__  ._____.                         .___       __                
+_/ ____\____      |  | |__\_ |__       __ ________   __| _/____ _/  |_  ___________ 
+\   __\/ __ \     |  | |  || __ \     |  |  \____ \ / __ |\__  \\   __\/ __ \_  __ \
+ |  | \  ___/     |  |_|  || \_\ \    |  |  /  |_> > /_/ | / __ \|  | \  ___/|  | \/
+ |__|  \___  >____|____/__||___  /____|____/|   __/\____ |(____  /__|  \___  >__|   
+           \/_____/            \/_____/     |__|        \/     \/          \/       
+`
+	fmt.Println("\033[31m" + asciiArt + "\033[0m")
+	printWorkDir()
 }
